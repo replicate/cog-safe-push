892a20cf6cf9fbf1adfcd6e307b9053d12868297
fofr <fofr@users.noreply.github.com>
2025-05-14 11:56:56 +0100

    Use same parser as other models

--- cog_safe_push/official_model.py
+++ cog_safe_push/official_model.py
@@ -1,12 +1,13 @@
 from . import cog
 from .log import info
+from .main import parse_model
 from .task_context import get_or_create_model
 
 
 def push_official_model(
     official_model: str, dockerfile: str | None, fast_push: bool = False
 ) -> None:
-    owner, name = official_model.split("/")
+    owner, name = parse_model(official_model)
     model = get_or_create_model(owner, name, "cpu")
     info(f"Pushing to official model {official_model}")
     cog.push(model, dockerfile, fast_push)
