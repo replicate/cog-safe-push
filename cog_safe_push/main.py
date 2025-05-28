e19f5497042a67e377ef5b7bb04a1bfdcff2c2a7
andreasjansson <andreas@replicate.ai>
2025-05-28 20:32:31 +0200

    formatting

--- cog_safe_push/main.py
+++ cog_safe_push/main.py
@@ -472,7 +472,6 @@ async def run_tasks(tasks: list[Task], parallel: int) -> None:
         log.error(f"Found {len(errors)} error(s):")
         for error, prediction_index in errors:
             prefix = "" if prediction_index is None else f"[{prediction_index}] "
-
             log.error(f"{prefix}{error}")
 
         sys.exit(1)
