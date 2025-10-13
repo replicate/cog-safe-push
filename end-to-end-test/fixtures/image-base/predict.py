98b7e60ec47b64e59148174d6b1bcab0fb8b6bc2
andreasjansson <andreas@replicate.ai>
2025-10-13 12:28:03 +0200

    document valid image types in fixture

--- end-to-end-test/fixtures/image-base/predict.py
+++ end-to-end-test/fixtures/image-base/predict.py
@@ -11,7 +11,7 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        image: Path = Input(description="Input image."),
+        image: Path = Input(description="Input image. Valid file types are: jpg, png, webp, bmp, gif (not animated)"),
         width: int = Input(description="New width.", ge=1, le=2000),
         height: int = Input(description="New height.", ge=1, le=1000),
     ) -> Path:
