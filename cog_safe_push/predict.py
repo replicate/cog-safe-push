653fc3df796adf5fe52aa88d551bf34554cb6ff9
andreasjansson <andreas@replicate.ai>
2025-11-07 15:10:46 +0100

    VERY IMPORTANT NO SVG FILES!

--- cog_safe_push/predict.py
+++ cog_safe_push/predict.py
@@ -140,7 +140,7 @@ If an input have format=uri and you decide to populate that input, you should us
 
 Make sure you pick an appropriate URL for the the input, e.g. pick one of the image examples below if the input expects an image. Also make sure you respect any hints or documentation about file types.
 
-IMPORTANT: DON'T USE SVG FILES UNLESS THE MODEL EXPLICITLY MENTINOS SVG SUPPORT!!! (most don't, but some svg-to-image, or vectorization models do).
+VERY IMPORTANT: DON'T USE SVG FILES UNLESS THE MODEL EXPLICITLY MENTINOS SVG SUPPORT!!! (most don't, but some svg-to-image, or vectorization models do).
 
 """
         + multimedia_example_files
